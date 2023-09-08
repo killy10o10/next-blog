3b46fa04c2fa41d58c1e022e8583b61ab70c3c47
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-09-08 01:08:52 +0000

    Make featured page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,11 @@
+import Featured from "@/components/featured/Featured";
+
 export default function Home() {
   return(
    <>
-      <div className="flex flex-col min-h-screen items-center justify-center p-3 lg:p-8">
-        <article className="prose lg:prose-xl text-inherit">
-          <h1 className="text-inherit">Garlic bread with cheese: What the science tells us</h1>
-          <p>
-            For years parents have espoused the health benefits of eating garlic bread with cheese to their
-            children, with the food earning such an iconic status in our culture that kids will often dress
-            up as warm, cheesy loaf for Halloween.
-          </p>
-          <p>
-            But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
-            springing up around the country.
-          </p>
-        </article>
+      <div className="prose lg:prose-xl max-w-5xl flex flex-col min-h-screen items-center justify-center p-3 lg:p-8 text-inherit">
+        <h1 className="text-inherit">Garlic bread with cheese: What the science tells us</h1>
+        <Featured />
       </div>
    </>
   );
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Featured() {
   return (
-    <div>
-      
+    <div className='grid md:grid-cols-2 gap-5 justify-center'>
+        <div className='flex items-center justify-end'>
+          <Image className='m-0 w-[100%] h-[100%]' src="/blog1.jpg" width={300} height={300} alt='blog1' />
+        </div>
+        <div className="prose text-inherit">
+          <h2 className="text-inherit">Garlic bread with cheese: What the science tells us</h2>
+          <p>
+            For years parents have espoused the health benefits of eating garlic bread with cheese to their
+            children, with the food earning such an iconic status in our culture that kids will often dress
+            up as warm, cheesy loaf for Halloween.
+          </p>
+          <button className='btn btn-neutral'>Read more</button>
+        </div>
     </div>
   )
 }
