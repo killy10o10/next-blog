8aeb0cfec957040f87172a87f8fa036d173a89ff
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-09-14 02:33:59 +0000

    wip

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 const singlePostPage = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen max-w-[1400px] text-inherit">
+      <div className='grid md:grid-cols-2'>
+        <div className='flex flex-col items-center justify-center'>
+          <h1 className="text-inherit tracking-wide leading-2 text-4xl lg:text-6xl font-black">Garlic bread with cheese: What the science tells us</h1>
+          <div>
+            
+          </div>
+        </div>
+        <div>
+          <Image src="/fashion.jpg" width={500} height={500} className='rounded-xl w-full h-[70%]' />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default singlePostPage
+export default singlePostPage;
