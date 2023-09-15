29b8f79a8a78e643ef3193e1358a4c106200039f
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-09-15 13:09:40 +0000

    Fix login component overload error

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,42 +1,62 @@
 "use client"
-import { signIn, useSession } from "next-auth/react"
-import { useRouter } from "next/navigation";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation"; // Changed from "next/navigation" to "next/router"
+import { useEffect } from "react";
 
 function LoginPage() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-  const {data, status} = useSession();
+  console.log(session, status)
 
-  const router = useRouter()
-
-  if(status === "loading") {
-    return <div className="text-center font-bold text-2xl text-yellow-400 min-h-screen flex justify-center items-center">Loading...</div>
-  }
-
-  if(status === "authenticated") {
-    router.push("/") 
-  }
-
-  console.log(data, status)
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/");
+    }
+  }, [status, router]);
 
   const handleGoogleLogin = () => {
     signIn("google");
-  }
-  const handleGithubleLogin = () => {
-    signIn("google");
-  }
-  const handleFacebookleLogin = () => {
-    signIn("google");
-  }
+  };
+
+  const handleGithubLogin = () => {
+    signIn("github"); 
+  };
+
+  const handleFacebookLogin = () => {
+    signIn("facebook"); 
+  };
 
   return (
     <div className="min-h-screen text-inherit flex flex-col justify-center">
-      <div className='flex gap-4 rounded-xl flex-col items-center'>
-        <button onClick={handleGoogleLogin} className='font-bold active:scale-95 transition shadow-lg text-xl w-4/6 lg:w-3/6 text-slate-100 bg-red-500 py-4 w-  rounded-lg'>Sign in with Google</button>
-        <button onClick={handleGithubleLogin} className='font-bold active:scale-95 transition shadow-lg text-xl w-4/6 lg:w-3/6 text-slate-100 bg-blue-gray-500 py-4 rounded-lg'>Sign in with GitHub</button>
-        <button onClick={handleFacebookleLogin} className='font-bold active:scale-95 transition shadow-lg text-xl w-4/6 lg:w-3/6 text-slate-100 bg-blue-500 py-4  rounded-lg'>Sign in with Facebook</button>
-      </div>
+      {status === "loading" ? ( // Show loading message while checking authentication
+        <div className="text-center font-bold text-2xl text-yellow-400 min-h-screen flex justify-center items-center">
+          Loading...
+        </div>
+      ) : (
+        <div className="flex gap-4 rounded-xl flex-col items-center">
+          <button
+            onClick={handleGoogleLogin}
+            className="font-bold active:scale-95 transition shadow-lg text-xl w-4/6 lg:w-3/6 text-slate-100 bg-red-500 py-4 w- rounded-lg"
+          >
+            Sign in with Google
+          </button>
+          <button
+            onClick={handleGithubLogin}
+            className="font-bold active:scale-95 transition shadow-lg text-xl w-4/6 lg:w-3/6 text-slate-100 bg-blue-gray-500 py-4 rounded-lg"
+          >
+            Sign in with GitHub
+          </button>
+          <button
+            onClick={handleFacebookLogin}
+            className="font-bold active:scale-95 transition shadow-lg text-xl w-4/6 lg:w-3/6 text-slate-100 bg-blue-500 py-4  rounded-lg"
+          >
+            Sign in with Facebook
+          </button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- src/components/authLink/AuthLink.jsx
+++ src/components/authLink/AuthLink.jsx
@@ -1,8 +1,8 @@
-import { signOut } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 
 function AuthLink() {
-  const status = "authenticated"
+  const {status} = useSession()
 
   const handleSignout = () => {
     signOut()
@@ -11,7 +11,7 @@ function AuthLink() {
   return (
     <>
      {
-      status === "notauthenticated" ? <Link className='gradient-text' href="/login">Login</Link> :
+      status === "unauthenticated" ? <Link className='gradient-text' href="/login">Login</Link> :
      <div className='flex items-center gap-2'>
         <Link className='gradient-text' href="/write">Write</Link>
         <span onClick={handleSignout} className='cursor-pointer gradient-text'>Logout</span> 
