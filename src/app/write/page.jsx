de192d0236975991b567676322c3431062e78edd
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-09-15 03:36:47 +0000

    Add write blog page
    Style write blog page and blog slug

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,9 +1,42 @@
-const WritePage = () => {
-  return (
-    <div className='min-h-screen'>
-      <p>WritePage</p>
-    </div>
-  )
-}
+'use client';
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import "react-quill/dist/quill.bubble.css"
+import {BsPlusCircle, BsImage, BsUpload} from 'react-icons/bs'
+import {FaPhotoVideo} from 'react-icons/fa';
 
-export default WritePage
+const WritePage = () => {
+  const [open , setOpen] = useState(false);
+  const [value , setValue] = useState('');
+
+  const handleOpen = () => setOpen(!open);
+
+  return (
+    <div className="min-h-screen relative">
+      <input type="text" placeholder='Heading Here' className='bg-transparent outline-none font-bold text-6xl p-12' />
+      <div className='flex items-center gap-5 absolute z-50 left-5 top-36'>
+        <button onClick={handleOpen} className='active:scale-95 transition'>
+          <BsPlusCircle className='text-4xl' />
+        </button>
+        {
+          open && 
+          <div className='flex gap-3'>
+            <button className='active:scale-95 transition border rounded-full p-2 border-green-500'>
+              <BsImage className='text-xl' />
+            </button>
+            <button className='active:scale-95 transition border rounded-full p-2 border-green-500'>
+              <BsUpload className='text-xl' />
+            </button>
+            <button className='active:scale-95 transition border rounded-full p-2 border-green-500'>
+              <FaPhotoVideo className='text-xl' />
+            </button>
+          </div>
+        }
+      </div>
+      <ReactQuill className='' theme='bubble' value={value} onChange={setValue} placeholder='Write a post...' />
+      <button className='absolute active:scale-95 transition shadow top-0 right-0 bg-green-500 font-bold text-slate-100 px-5 py-2 rounded-full'>Publish</button>
+    </div>
+  );
+};
+
+export default WritePage;
--- src/components/authLink/AuthLink.jsx
+++ src/components/authLink/AuthLink.jsx
@@ -1,13 +1,16 @@
 import Link from 'next/link'
 
 function AuthLink() {
-  const status = "notauthenticated"
+  const status = "authenticated"
 
   return (
     <>
      {
       status === "notauthenticated" ? <Link className='gradient-text' href="/login">Login</Link> :
-       <span className='cursor-pointer gradient-text'>Logout</span> 
+     <div className='flex items-center gap-2'>
+        <Link className='gradient-text' href="/write">Write</Link>
+        <span className='cursor-pointer gradient-text'>Logout</span> 
+     </div>
      } 
     </>
   )
