f3677d141ab314173baec360d1e62a23ac130c6c
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-09-18 02:29:49 +0000

    add files to .gitignore

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,28 +1,34 @@
-import CategoryButton from '../categoryButton/CategoryButton'
-
+import CategoryButton from '../categoryButton/CategoryButton';
 
 const getData = async () => {
-  const res = await  fetch("http://localhost:3000/api/categories", {
-    cache: "default",
-  })
-  if(!res.ok) {
-      throw new  Error("Failed")
+  const res = await fetch('http://localhost:3000/api/categories', {
+    cache: 'default',
+  });
+  if (!res.ok) {
+    throw new Error('Failed');
   }
-  return res.json()
-}
+  return res.json();
+};
 
 const CategoryList = async () => {
-  const data = await  getData();
-  return (    <>
-    <div className='prose text-inherit max-w-full my-4'>
-      <h2 className='text-inherit'>Categories</h2>
-      <div className='flex flex-wrap gap-5 justify-between items-center'>
-        {
-          data?.map((item) => <CategoryButton key={item._id} categoryImage={item.img} categoryBG={`bg-${item.title}`} categoryText={item.title}/> )
-        }
+  const data = await getData();
+  return (
+    <>
+      <div className="prose text-inherit max-w-full my-4">
+        <h2 className="text-inherit">Categories</h2>
+        <div className="flex flex-wrap gap-5 justify-between items-center">
+          {data?.map((item) => (
+            <CategoryButton
+              key={item._id}
+              categoryImage={item.img}
+              categoryBG={`bg-${item.title}`}
+              categoryText={item.title}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  </>)
-}
+    </>
+  );
+};
 
-export default CategoryList
+export default CategoryList;
