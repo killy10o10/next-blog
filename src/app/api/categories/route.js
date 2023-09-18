f041c89fad91910a33debcdcbeaa4b79dea7cb9f
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-09-18 02:16:29 +0000

    create categories api route
    Dynamically fetch categories and render them on home page

---
+++ src/app/api/categories/route.js
@@ -0,0 +1,12 @@
+import prisma from '@/utils/connect';
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories, { status: 200 }));
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(JSON.stringify({ message: 'Something went wrong!' }, { status: 500 }));
+  }
+};
--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,21 +1,28 @@
 import CategoryButton from '../categoryButton/CategoryButton'
 
-function CategoryList() {
-  return (
-    <>
-      <div className='prose text-inherit max-w-full my-4'>
-        <h2 className='text-inherit'>Categories</h2>
-        <div className='flex flex-wrap gap-5 justify-between items-center'>
-          <CategoryButton categoryImage="/coding.jpg" categoryBG={`bg-coding`} categoryText="coding" />
-          <CategoryButton categoryImage="/style.jpg" categoryBG="bg-style" categoryText="style" />
-          <CategoryButton categoryImage="/fashion.jpg" categoryBG="bg-fashion" categoryText="fashion" />
-          <CategoryButton categoryImage="/food.png" categoryBG="bg-food" categoryText="food" />
-          <CategoryButton categoryImage="/culture.jpg" categoryBG={"bg-culture"}categoryText="culture" />
-          <CategoryButton categoryImage="/travel.jpeg" categoryBG="bg-travel" categoryText="travel" />
-        </div>
+
+const getData = async () => {
+  const res = await  fetch("http://localhost:3000/api/categories", {
+    cache: "default",
+  })
+  if(!res.ok) {
+      throw new  Error("Failed")
+  }
+  return res.json()
+}
+
+const CategoryList = async () => {
+  const data = await  getData();
+  return (    <>
+    <div className='prose text-inherit max-w-full my-4'>
+      <h2 className='text-inherit'>Categories</h2>
+      <div className='flex flex-wrap gap-5 justify-between items-center'>
+        {
+          data?.map((item) => <CategoryButton key={item._id} categoryImage={item.img} categoryBG={`bg-${item.title}`} categoryText={item.title}/> )
+        }
       </div>
-    </>
-  )
+    </div>
+  </>)
 }
 
 export default CategoryList
