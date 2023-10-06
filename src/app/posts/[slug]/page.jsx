e4c723591c2a093ed42861cabf59c6ec76bc4953
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-10-06 13:24:20 +0000

    successfully fetch posts
    format post dates and description with <br> tags

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -15,52 +15,55 @@ const getData = async (slug) => {
 
 const singlePostPage = async ({params}) => {
   const {slug} = params
-  const postData = await getData(slug);
-  console.log(postData)
+  const { post } = await getData(slug);
 
   const insertBrTags = (text, interval) => {
     if (typeof text !== 'string') {
       return ''; // Return an empty string or handle the error as needed
     }
-  
     const words = text.split(' ');
     let modifiedText = [];
-  
     for (let i = 0; i < words.length; i++) {
       modifiedText.push(words[i]);
-  
       // Check if we need to insert a <br> tag
       if ((i + 1) % interval === 0 && i < words.length - 1) {
-        modifiedText.push('<br>');
+        modifiedText.push(`<br/> <br />`);
       }
     }
-  
     return modifiedText.join(' ');
+  }
+
+  function formatDateToWords(dateString) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
   }
 
   return (
     <div className="min-h-screen max-w-[1400px] text-inherit">
       <div className='grid md:grid-cols-2 gap-10 md:gap-0 mt-5'>
         <div className='flex flex-col gap-5 justify-center'>
-          <h1 className="text-inherit tracking-wide leading-2 text-4xl lg:text-6xl font-black">{postData?.title}</h1>
+          <h1 className="text-inherit tracking-wide leading-2 text-4xl lg:text-6xl font-black">{post?.title}</h1>
           <div className='flex items-center gap-4'>
-            <Image className='rounded-full h-16 w-16 object-cover' src="/culture.jpg" alt='editors pick' width={35} height={35} />
+            {post && <Image className='rounded-full h-16 w-16 object-cover' src={post.user.image} alt='editors pick' width={35} height={35} />}
             <div>
-              <h4 className='text-inherit font-semibold'>Kingsley Quami Okine</h4>
-              <span className="dark:text-gray-600 text-gray-400 font-semibold">25 April 2023</span>
+              <h4 className='text-inherit font-semibold'>{post?.user.name}</h4>
+              {post && <span className="dark:text-gray-600 text-gray-400 font-semibold">{formatDateToWords(post.createdAT.substring(0, 10))}</span>}
             </div>
           </div>
         </div>
         <div className='flex flex-col justify-center'>
-         {postData && <Image src={postData.img} alt='featured-image' width={500} height={300} className='rounded-xl w-full h-[79%]' />}
+         {post && <Image src={post.img} alt='featured-image' width={500} height={350} className='rounded-xl w-full h-[350px] mb-10' />}
         </div>
       </div>
 
       <div className='grid md:grid-cols-7 gap-12 text-inherit'>
           <div className='col-span-5 flex flex-col' >
-            {
-              postData && insertBrTags(postData.desc, 48)
-            }
+          {post && (
+            <div
+              dangerouslySetInnerHTML={{ __html: insertBrTags(post.desc, 48) }}
+            />)
+          }
             <br />
             <Comment />
           </div>
