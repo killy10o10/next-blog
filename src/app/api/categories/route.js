4d00705cbbb5f28a2401f08528c31ffae44dbdea
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-10-18 16:37:59 +0000

    Remove console messages from server actions

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -6,7 +6,8 @@ export const GET = async () => {
     const categories = await prisma.category.findMany();
     return new NextResponse(JSON.stringify(categories, { status: 200 }));
   } catch (error) {
-    console.log(error);
-    return new NextResponse(JSON.stringify({ message: 'Something went wrong!' }, { status: 500 }));
+    return new NextResponse(
+      JSON.stringify({ message: 'Something went wrong!' }, { status: 500 })
+    );
   }
 };
--- src/app/api/posts/[slug]/route.js
+++ src/app/api/posts/[slug]/route.js
@@ -8,13 +8,12 @@ export const GET = async (req, { params }) => {
   try {
     const post = await prisma.post.update({
       where: { slug },
-      data: {views: {increment: 1}},
+      data: { views: { increment: 1 } },
       include: { user: true },
     });
 
     return new NextResponse(JSON.stringify({ post }, { status: 200 }));
   } catch (error) {
-    console.log(error);
     return new NextResponse(
       JSON.stringify({ message: 'Something went wrong!' }, { status: 500 })
     );
